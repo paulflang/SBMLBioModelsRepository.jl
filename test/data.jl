bd4a8dcf47232e8cc29c8e315e28895f66978d0a
Anand <anandj@uchicago.edu>
2021-04-07 23:06:11 -0400

    stop testing biomd and fix sbml

--- test/data.jl
+++ test/data.jl
@@ -4,14 +4,15 @@ using Test
 
 # test we get all biomodels sbml files
 # biomodels() # don't run all the time
-datadir = joinpath(@__DIR__, "../data/")
-@test ispath(datadir)
+# datadir = joinpath(@__DIR__, "../data/")
+# @test ispath(datadir)
 
-fns = readdir(joinpath(datadir, "biomd/"))
-biomd_df = CSV.read(joinpath(datadir, "sbml_biomodels.csv"), DataFrame)
+# fns = readdir(joinpath(datadir, "biomd/"))
+# biomd_df = CSV.read(joinpath(datadir, "sbml_biomodels.csv"), DataFrame)
 
-@test length(fns) == nrow(biomd_df) # 2216
+# @test length(fns) == nrow(biomd_df) # 2216
 
 # test suite stuff
+sbml_test_suite()
 suite_fns = get_sbml_suite_fns()
 @test isfile(suite_fns[1])
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,4 @@ using Test
 
 @testset "SBMLBioModelsRepository.jl" begin
     @testset "data" begin include("data.jl") end
-    @testset "sciml" begin include("sciml.jl") end
 end
