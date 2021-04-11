7441520fad7bcd67c0b2ec3634436014ec71a99b
Anand <anandj@uchicago.edu>
2021-04-11 12:11:10 -0700

    disable biomd tests

--- test/data.jl
+++ test/data.jl
@@ -3,21 +3,21 @@ using CSV, DataFrames
 using Test
 
 # test we get all biomodels sbml files
-biomodels()
-datadir = joinpath(@__DIR__, "../data/")
-@test ispath(datadir)
+# biomodels()
+# datadir = joinpath(@__DIR__, "../data/")
+# @test ispath(datadir)
 
-biomd_fns = readdir(joinpath(datadir, "biomd/"))
-biomd_df = CSV.read(joinpath(datadir, "sbml_biomodels.csv"), DataFrame)
+# biomd_fns = readdir(joinpath(datadir, "biomd/"))
+# biomd_df = CSV.read(joinpath(datadir, "sbml_biomodels.csv"), DataFrame)
 
-@test length(biomd_fns) == nrow(biomd_df) # 2216
+# @test length(biomd_fns) == nrow(biomd_df) # 2216
 
 # test suite stuff
 sbml_test_suite()
 suite_fns = get_sbml_suite_fns()
 @test isfile(suite_fns[1])
 
-fns = vcat(biomd_fns, suite_fns)
+# fns = vcat(biomd_fns, suite_fns)
 
 using Pkg
 Pkg.add(url="https://github.com/LCSB-BioCore/SBML.jl/")
@@ -25,7 +25,7 @@ using SBML
 
 good = []
 bad = []
-for fn in fns
+for fn in suite_fns
     try 
         m = readSBML(fn)
         push!(good, fn => m)
