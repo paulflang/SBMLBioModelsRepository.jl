f6272409826e97aafed47b6e12209aa7d686b29c
anand jain <anandj@uchicago.edu>
2021-06-02 12:45:05 -0700

    mkdir(logs) and rm SBML

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using SBMLBioModelsRepository
 using CSV, DataFrames, Glob
 using Test
 
+!isdir("logs/") && mkdir("logs/")
+
 @testset "SBMLBioModelsRepository.jl" begin
     @testset "data" begin include("data.jl") end
     @testset "lower" begin include("lower.jl") end
